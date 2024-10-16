e9325e3ca4590052184f5f8ba76605e9f535dc64
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-10-16 16:22:14 +0530

    nikita code

---
+++ test.py
@@ -0,0 +1,46 @@
+# st = "upflairs pvt ltd jaipur rajasthan" 
+# print(st.upper()) 
+# print(st.lower())
+# print(st.title())  # first letter of word 
+# print(st.capitalize())
+# print(len(st)) 
+# print(st.count('p')) 
+# print(st.count('Jaipur')) 
+
+
+## boolean 
+# var = True 
+# var2 = False 
+
+# print(type(var))
+# print(type(var2))  
+
+# var = False  
+# print(type(var))
+
+
+# st = "upflairs pvt ltd jaipur rajasthan" 
+# print(st.endswith('n'))
+# print(st.endswith('rajasthan')) 
+# print(st.startswith('upfr'))
+# print(st.index('ltds'))  # return the position of the item  
+# print(st.find('ltds'))
+
+
+# st = "     upflairs      "
+# print(st.center(16,"_"))  # ____upflairs____ 
+# print(st.ljust(12,"_"))
+# print(st.rjust(12,'_'))
+# print(st)
+# print(st.strip())
+# print(st.lstrip())
+# print(st.rstrip())
+
+
+st = " "
+# print(st.isupper())
+# print(st.islower())
+# print(st.isalpha())     # 
+# print(st.isalnum())
+# print(st.isdigit()) 
+print(st.isspace())
---
+++ test2.py
@@ -0,0 +1,10 @@
+st = "upflairs pvt ltd jaipur" 
+# print(st.isalpha()) 
+# print(st.isalnum()) 
+# print(st.replace("upflairs","flipkart")) 
+# print(st.replace("u","p")) 
+# print(st)
+st2 = st.replace("jaipur","")
+print(st2)
+print(st)
+
