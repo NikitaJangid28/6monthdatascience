5ffe61a6ea5558ebe61460e80874c7ceab44f68b
Ranjit-singh-786 <jiradhey402@gmail.com>
2025-01-23 15:56:35 +0530

    cli done

---
+++ nikita/sql/day1.sql
@@ -0,0 +1,18 @@
+show databases;
+
+create database nikita;
+drop database nikita;
+
+create database civil;
+
+use civil;
+
+create table studentdata (stu_roll_no int, stu_name varchar(25), stu_age int, stu_city varchar(30));
+show tables;
+drop table studentdata;
+
+desc studentdata;
+select * from studentdata;
+
+
+
